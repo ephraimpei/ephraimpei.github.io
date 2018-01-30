b1b26c7a30b71b6a8b3c58990f14c64898fdf5d7
ephraimpei <ephraim.pei@vice.com>
2018-01-29 22:33:37 -0500

    update contact page spec

---
+++ src/app/pages/contact/contact.page.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { ContactPage } from './contact.page';
+import { ContactForm } from '../../components';
+
+describe('ContactPage', () => {
+  let component: ContactPage;
+  let fixture: ComponentFixture<ContactPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ContactPage, ContactForm ],
+      imports: [ FormsModule, RouterTestingModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContactPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
